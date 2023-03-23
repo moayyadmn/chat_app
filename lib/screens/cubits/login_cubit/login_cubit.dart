65c19fe6add49dddc11eaf16e9de5bc912c96547
moayyadmn <moayyadvvv@gmial.com>
2023-03-23 17:45:48 +0300

    First_commit

---
+++ lib/screens/cubits/login_cubit/login_cubit.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+part 'login_state.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  LoginCubit() : super(LoginInitial());
+  Future<void> loginUser(
+      {required String email, required String password}) async {
+    emit(LoginLoading());
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      emit(LoginSuccess());
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        emit(LoginFailure(errMessage: 'user-not-found'));
+      } else if (e.code == 'wrong-password') {
+        emit(LoginFailure(errMessage: 'wrong-password'));
+      }
+    } catch (e) {
+      emit(LoginFailure(errMessage: 'an error has occurred'));
+    }
+  }
+}
